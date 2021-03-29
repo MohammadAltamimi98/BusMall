6da15ac265790cdadfaa466489f313565001b144
Mohammad Altamimi <m98altamimi@gmail.com>
2021-03-29 15:55:42 +0300

    a

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -2,18 +2,20 @@
 let leftIndex;
 let middleIndex;
 let rightIndex;
-let results;
-let clickNum=0;
+let votesArray=[];
+let viewsArray=[];
+let attempt=1;
+let maxAttempts=5;
 
 
 
 
 
-const imgSection=document.getElementById('images-section');
+const imgSection=document.getElementById('imagesSection');
 const leftImg=document.getElementById('left-img');
 const middleImg=document.getElementById('middle-img');
 const rightImg=document.getElementById('right-img');
-const result=document.getElementById('myList');
+
 
 const ItemsNames=['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 
@@ -24,7 +26,7 @@ function getRandomArNumber(min, max) {
 }
 
 
-
+// constructor function
 function Item(name){
   this.name = name;
   this.path = `img/${name}.jpg`;
@@ -47,7 +49,7 @@ function render(){
   leftImg.src = Item.all[leftIndex].path;
   leftImg.alt=Item.all[leftIndex].name;
   leftImg.title=Item.all[leftIndex].name;
-  Item.all[leftIndex].views+=1;
+  Item.all[leftIndex].views++;
 
 
   do { middleIndex=getRandomArNumber(0,Item.all.length-1);
@@ -55,7 +57,7 @@ function render(){
     middleImg.alt=Item.all[middleIndex].name;
     middleImg.title=Item.all[middleIndex].name;
   } while(middleIndex===leftIndex);
-  Item.all[middleIndex].views+=1;
+  Item.all[middleIndex].views++;
 
   do{
     rightIndex=getRandomArNumber(0,Item.all.length-1);
@@ -63,84 +65,94 @@ function render(){
     rightImg.alt=Item.all[rightIndex].name;
     rightImg.title=Item.all[rightIndex].name;}
   while(rightIndex===middleIndex|| rightIndex=== leftIndex);
-  Item.all[rightIndex].views+=1;
+  Item.all[rightIndex].views++;
 
 
 
 
 
-  console.log(leftIndex);
-  console.log(middleIndex);
-  console.log(rightIndex);
+  // console.log(leftIndex);
+  // console.log(middleIndex);
+  // console.log(rightIndex);
 }
 
 
 imgSection.addEventListener('click', handleClick);
 
 function handleClick (event){
-  if (event.target.id !== 'images-section')
-  {
-    if (event.target.id === rightImg.id)
-    {
-      Item.all[rightIndex].votes++;
+  if (event.target.id !== 'imagesSection'){
+    if (attempt<maxAttempts){
+      attempt++;
+      if (event.target.id === rightImg.id){
+        Item.all[rightIndex].votes++;
+      }
+      // debugger;
+      if (event.target.id === middleImg.id){
+        Item.all[middleIndex].votes++;
+      }
+
+      else {
+        Item.all[leftIndex].votes++;
+      }
+      render();
+
+    } else{
+      if (event.target.id === rightImg.id){
+        Item.all[rightIndex].votes++;
+      }
+      // debugger;
+      if (event.target.id === middleImg.id){
+        Item.all[middleIndex].votes++;
+      }
+
+      else {
+        Item.all[leftIndex].votes++;
+      }
+
+      let ulEl=document.getElementById('listResult');
+      let liEl;
+      for(let i=0;i<Item.all.length;i++){
+        votesArray.push(Item.all[i].votes);
+        viewsArray.push(Item.all[i].views);
+        // console.log(votesArray,viewsArray);
+
+        liEl=document.createElement('li');
+        liEl.textContent=`${Item.all[i].name} has ${Item.all[i].votes} votes and ${Item.all[i].views} views.`;
+        ulEl.appendChild(liEl);
+      }
+      imagesSection.removeEventListener('click', handleClick);
+      chartRender();
+
     }
-
-    else if (event.target.id === middleImg.id)
-    {
-      Item.all[middleIndex].votes++;
-    }
-
-    else {
-      // (event.target.id === leftImg.id)
-      Item.all[leftIndex].votes++;
-
-    }
-
-
-
-    console.table(Item.all);
-    render();
-
-    clickNum=clickNum+1;
-
-    if(clickNum===25){
-      results=document.createElement('button');
-      result.appendChild(results);
-      //   console.log(result,results);
-      results.textContent='View Results';
-      results.addEventListener('click',Calculate);
-      // reset=document.createElement('button');
-      // result.appendChild(reset);
-      // //   console.log(result,results);
-      // reset.textContent='Reset';
-      // results.addEventListener('click',reset);
-    }
-
   }
-
 }
 
+function chartRender(){
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+  // The type of chart we want to create
+    type: 'bar',
 
-function Calculate(event){
-  const ul = document.createElement('ul');
-  result.appendChild(ul);
-  for(let i=0; i<Item.all.length; i++){
-    const li = document.createElement('li');
-    ul.appendChild(li);
-    li.textContent = `${Item.all[i].name} had ${Item.all[i].votes} votes, and was seen ${Item.all[i].views} times.`;
-  }
+    // The data for our dataset
+    data: {
+      labels: ItemsNames,
+      datasets: [{
+        label: 'Views',
+        backgroundColor: 'rgb(104, 241, 209)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: viewsArray,
 
-  clickNum=0;
+      },
+      {
+        label: 'Votes',
+        backgroundColor: 'rgb(0, 0, 0)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: votesArray,
+      }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
 }
-
-
-
-// function reset (event){
-
-//   clickNum=0;
-// }
-
 render();
-
-
-
